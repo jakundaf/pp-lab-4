3040c29ea9888e8aa8525c1a12e78367a822a70a
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-07 18:22:02 +0200

    Add files via upload

---
+++ employees/Worker.java
@@ -0,0 +1,31 @@
+package employees;
+
+public class Worker extends Employee {
+
+    public String position;
+
+    public Worker(String fullName, double salary, String position) {
+        super(fullName, salary);
+        this.position = position;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "fullName'" + getName() +
+                "', salary=" + getSalary() +
+                ", Position'" + getPosition() +
+                "'}";
+    }
+}
